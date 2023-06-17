eb2f5ec58c1d2910f05ac26e0def031d04d0bf8c
Yarrut Franken <yarrutfranken@Yarruts-MacBook-Pro-2.local>
2023-06-17 12:22:10 +0200

    update base requests setup

--- web/frontend/components/OrderList.jsx
+++ web/frontend/components/OrderList.jsx
@@ -29,7 +29,12 @@ export function OrderList() {
         const response = await fetch("/api/orders");
 
         if (response.ok) {
-            await refetchProductCount();
+            await refetchProductCount().then((res) => {
+                const filtered = res.data.orders.body.data.orders.edges.filter((order) => {
+                    return !order.node.customer.tags.includes('b2b');
+                })
+                setOrders(filtered)
+            });
             setToastProps({ content: "5 products created!" });
         } else {
             setIsLoading(false);
@@ -45,11 +50,47 @@ export function OrderList() {
     }, [])
 
     useEffect(() => {
-        console.log(data)
-    }, [data])
+        console.log(orders)
+    }, [orders])
+
     return (
-        <>
-            test
-        </>
+        <div class="dashboard-wrapper">
+            <table className="">
+                <thead>
+                    <tr>
+                        <td>
+                            #
+                        </td>
+                        <td>
+                            date
+                        </td>
+                        <td>
+                            customer
+                        </td>
+                        <td>
+                            total
+                        </td>
+                        <td>
+                            payment status
+                        </td>
+                        <td>
+                            items
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders && orders.map((order) => (
+                        <tr>
+                            <td>{order.node.name}</td>
+                            <td>{order.node.createdAt}</td>
+                            <td>{order.node.customer.firstName} {order.node.customer.lastName}</td>
+                            <td>{`${order.node.totalPriceSet.presentmentMoney.amount} ${order.node.totalPriceSet.presentmentMoney.currencyCode}`}</td>
+                            <td>{order.node.fullyPaid && 'fully Paid'}</td>
+                            <td>/</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
--- web/getOrderList.js
+++ web/getOrderList.js
@@ -2,28 +2,73 @@ import { GraphqlQueryError } from "@shopify/shopify-api";
 import shopify from "./shopify.js";
 
 export default async function getOrderList(session) {
-    const client = new shopify.api.clients.Graphql({ session });
+  const client = new shopify.api.clients.Graphql({ session });
 
-    try {
-        const data = await client.query({
-            data: `query {
-            orders(first: 10) {
-              edges {
-                node {
-                  id
+  try {
+    const data = await client.query({
+      data: `query {
+              orders(first: 10) {
+                edges {
+                  node {
+                    id
+                    name
+                    email
+                    createdAt
+                    customer {
+                      id
+                      firstName
+                      lastName
+                      tags
+                    }
+                    totalPriceSet {
+                      presentmentMoney {
+                        amount
+                        currencyCode
+                      }
+                    }
+                    fullyPaid
+                    billingAddress {
+                      address1
+                      address2
+                      city
+                      company
+                      country
+                      countryCode
+                      firstName
+                      lastName
+                      name
+                      id
+                      formatted
+                      latitude
+                      longitude
+                      province
+                      provinceCode
+                      zip
+                    }
+                    totalPriceSet {
+                      presentmentMoney{
+                        amount
+                        currencyCode
+                      }
+                      shopMoney{
+                        amount
+                        currencyCode
+                      }
+                    }
+                  }
                 }
               }
             }
-          }`,
-        });
-        return data;
-    } catch (error) {
-        if (error instanceof GraphqlQueryError) {
-            throw new Error(
-                `${error.message}\n${JSON.stringify(error.response, null, 2)}`
-            );
-        } else {
-            throw error;
-        }
+            `,
+    });
+    return data;
+  } catch (error) {
+    if (error instanceof GraphqlQueryError) {
+      throw new Error(
+        `${error.message}\n${JSON.stringify(error.response, null, 2)}`
+      );
+    } else {
+      throw error;
     }
+  }
 }
